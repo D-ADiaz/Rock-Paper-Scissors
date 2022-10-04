1a6d6a8c21280953f7779f14957748d9066d3c06
Danny <ddiaz@scu.edu>
2022-10-04 04:14:20 -0700

    Add Display Function
    - Revise game functions

--- js-file.js
+++ js-file.js
@@ -1,12 +1,94 @@
+/* Gameplay Functions */
+function computerPlay(){
+    const compChoice = ["rock","paper","scissors"];
+    const random = Math.floor(Math.random()*3);
+
+    return compChoice[random];
+}
+
+function playRound(playerSelection,computerSelection){
+
+    if(playerSelection === computerSelection){
+        return "Tie";
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){ 
+        return "Player wins!";
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "Player wins!";
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "Player wins!";
+    }
+    else{
+        return "Computer wins!";
+    }
+};
+
+function changeScore(winner){
+    if (winner === "Player wins!"){
+        playerWins ++;
+    }
+    else if (winner === "Computer wins!"){
+        compWins ++;
+    }
+    roundResult = winner;
+
+    checkForWinner(playerWins,compWins);
+}
+
+function checkForWinner(score1,score2){
+
+    if (score1 === 3){
+
+        alert("Player Wins The Game!");
+
+        resetGame();
+    }
+    else if (score2 ===3){
+        alert("Computer Wins The Game");
+
+        resetGame();
+    }
+}
+
+function resetGame(){
+    compWins = 0;
+    playerWins = 0;
+}
+
+function playGame(roundResult){
+
+    changeScore(roundResult);
+
+    playerDisp.innerHTML = playerWins;
+    compDisp.innerHTML = compWins;
+    roundDisp.innerHTML = roundResult;
+
+}
+
+/* Display Variables */
+let roundResult;
+let compWins = 0;
+let playerWins = 0;
+const playerDisp = document.getElementById("playerDisplay");
+const compDisp = document.getElementById("computerDisplay");
+const roundDisp = document.getElementById("roundDisplay");
+
+
 /* button functionality */
 const rockBtn = document.querySelector("#rock");
-rockBtn.addEventListener("click",() => console.log(playRound("rock",computerPlay())));
-
 const paperBtn = document.querySelector("#paper");
-paperBtn.addEventListener("click",() => console.log(playRound("paper",computerPlay())));
-
 const scissorsBtn = document.querySelector("#scissors");
-scissorsBtn.addEventListener("click",() => console.log(playRound("scissors",computerPlay())));
+
+
+rockBtn.addEventListener("click",() => playGame(playRound("rock",computerPlay())));
+
+paperBtn.addEventListener("click",() =>  playGame(playRound("paper",computerPlay())));
+
+scissorsBtn.addEventListener("click",() => playGame(playRound("scissors",computerPlay())));
+
+
 
 /* button animation */
 const btns = document.querySelectorAll("button");
@@ -16,6 +98,7 @@ btns.forEach(btns => btns.addEventListener("click", () => btns.classList.add("cl
 function removeTransition(e){
     if (e.propertyName !== "transform") return;
     this.classList.remove("clicked");
+    console.log(e);
 }
 
 btns.forEach(btns => btns.addEventListener("transitionend",removeTransition));
